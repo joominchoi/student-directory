f9778e5deca94411afa524ec6d661d0129604e2a
JooMin Choi <joominchoi23@gmail.com>
2021-05-21 15:27:42 +0100

    modified program to print all students using while instead of each

--- directory.rb
+++ directory.rb
@@ -23,10 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    if student[:name].length < 12
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+  count = 0
+  while students.length > count
+    puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    count += 1
   end
 end
 
